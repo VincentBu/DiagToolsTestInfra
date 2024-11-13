69ccb87c499a6201ab8092faf3368aba32b00594
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2024-11-13 15:59:18 +0800

    update core functionality

---
+++ src/DiagToolsValidator/DiagToolsValidator.Core/CoreFunctionality/CommandInvoker.fs
@@ -0,0 +1,37 @@
+﻿namespace DiagToolsValidationToolSet.Core.Utility
+
+open System.Collections.Generic
+open System.Diagnostics
+
+open DiagToolsValidator.Core.CoreFunctionality.TypeDefinition
+
+module CommandInvoker =
+
+    let InvokeCommand (env: Dictionary<string, string>) (workingDirectory: string) (fileName: string) (argument: string) =
+        let runinfo = new CommandRunInfo()
+        runinfo.Command <- $"{fileName} {argument}"
+        runinfo.StandardOutput <- ""
+        runinfo.StandardError <- ""
+
+        use proc = new Process()
+        proc.StartInfo.FileName <- fileName
+        proc.StartInfo.Arguments <- argument
+        proc.StartInfo.RedirectStandardOutput <- true
+        proc.StartInfo.RedirectStandardError <- true
+        proc.StartInfo.UseShellExecute <- false
+
+        proc.StartInfo.WorkingDirectory <- workingDirectory
+
+        for kp in env do
+            proc.StartInfo.EnvironmentVariables[kp.Key] <- kp.Value
+
+        proc.OutputDataReceived.Add(fun _ -> 
+            let line = proc.StandardOutput.ReadLine()
+            runinfo.StandardOutput <- $"{runinfo.StandardOutput}{line}\n"
+            printfn "%A" line)
+        proc.ErrorDataReceived.Add(fun _ -> 
+            let line = proc.StandardError.ReadLine()
+            runinfo.StandardError <- $"{runinfo.StandardError}{line}\n"
+            printfn "%A" line)
+        
+        runinfo
---
+++ src/DiagToolsValidator/DiagToolsValidator.Core/CoreFunctionality/Common.fs
@@ -0,0 +1,54 @@
+﻿namespace DiagToolsValidator.Core.CoreFunctionality
+
+open System.IO
+open System.Reflection
+open System.IO.Compression
+open System.Formats.Tar
+
+open TypeDefinition
+
+module Common =
+    let IsNullOrEmptyString (str: string) : bool = 
+        if str = null || str.Length = 0
+        then true
+        else false
+
+
+    let CopyEmbeddedFile (resourceName: string) (destination: string) : IOAction<unit> =
+        let assembly = Assembly.GetExecutingAssembly()
+        use sr = assembly.GetManifestResourceStream(resourceName)
+        use sw = File.OpenWrite(destination)
+        IOAction (fun () -> sr.CopyTo(sw))
+
+
+    let CopyFile (source: string) (destination: string) : IOAction<unit> =
+        let realDestinationPath =
+            if Directory.Exists destination
+            then
+                let fileName = Path.GetFileName(source)
+                Path.Combine(destination, fileName)
+            else
+                destination
+        IOAction (fun () -> File.Copy(source, realDestinationPath))
+
+
+    let CreateDirectory (path: string) : IOAction<DirectoryInfo> =
+        IOAction (fun () -> Directory.CreateDirectory path)
+
+
+    let DecompressGzip (gzipPath: string) (destination: string) =
+        IOAction (fun () -> 
+                    use originalFileStream = File.OpenRead gzipPath
+                    use decompressedFileStream = File.Create destination
+                    use decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress)
+                    decompressionStream.CopyTo(decompressedFileStream))
+
+
+    let DecompressTar (tarPath: string) (destinationFolder: string) = 
+        IOAction (fun () -> 
+            TarFile.ExtractToDirectory(tarPath, destinationFolder, true))
+
+
+    let DecompressZip (zipPath: string) (destinationFolder: string) = 
+        IOAction (fun () -> 
+            ZipFile.ExtractToDirectory(zipPath, destinationFolder))
