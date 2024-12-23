2284855d3091280b9c8d4854a53c46e5721f01b7
Vincent Bu (Centific Technologies Inc) <v-bibu@microsoft.com>
2024-12-23 17:14:55 +0800

    remove python script

--- src/DiagToolsValidatorPython/meta.py
+++
@@ -1,3 +0,0 @@
-import os
-
-script_root = os.path.dirname(os.path.abspath(__file__))
--- src/DiagToolsValidatorPython/test.py
+++
@@ -1,9 +0,0 @@
-import traceback
-
-ex = Exception('exp')
-
-print(f'throw ex: {ex}\n{traceback.print_exc()}')
-
-ex.add_note('add additional info')
-
-print(f'throw ex: {ex}\n{traceback.print_exc()}')
